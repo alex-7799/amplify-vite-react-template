d03323ff150d4b2c6c698dfcfedc55b445d5e81c
alex-7799 <116727987+alex-7799@users.noreply.github.com>
2024-05-30 23:59:55 +1000

    hot fix for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
